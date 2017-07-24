dab203e880a747600022012a630e35fec08f7b74
Cherryvita91 <cherryvita91@gmail.com>
2017-07-24 15:32:31 +0300

    add js

---
+++ js/js_for_lp.js
@@ -0,0 +1,25 @@
+$(document).ready(function(e) 
+
+{
+             
+
+
+
+//изменение цвета кнопки при наведении//
+
+
+$('.buttonstyle').click(
+
+	function()
+
+	{
+
+ $('.buttonstyle').addClass("buttonstyle-bg")
+	}
+
+);
+
+
+
+});
+
