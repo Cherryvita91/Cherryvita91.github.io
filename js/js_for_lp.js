d924c8d8a0d63ff9477e93532c395b54b461283e
Cherryvita91 <cherryvita91@gmail.com>
2017-07-31 13:27:27 +0300

    js scroll

--- js/js_for_lp.js
+++ js/js_for_lp.js
@@ -30,22 +30,40 @@ $(document).ready(function(e) {
 
 
 $('.buttonstyle').mouseenter(
-		function(){
-			$('.tooltip_first').show(300);
-		}
-
-
-		);
+function() {
+    $('.tooltip').show(300);
+    console.log(event.type);
+    console.log(event.target);
+}
+    );
 
 		$('.buttonstyle').mouseout(
 		function(){
 			$('.tooltip_first').hide(300);
 		}
 
-
 		);
 
-	
+$('.list-menu a').click(function(){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        $('html, body').animate({
+            scrollTop: destination
+        }, 1000);
+    });
+
+
+$('.buttonstyle').click(function(){
+
+$(".popup-wrap").fadeIn(300);
+
+});
+$('#close').click(function(){
+$(".popup-wrap").fadeOut(700);
+
+
+});
+
 
 
 });
